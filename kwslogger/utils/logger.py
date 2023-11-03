0f16288b7ef8ec48a5648d8e14f09b469acce866
kWAY <55437151+kWAYTV@users.noreply.github.com>
2023-11-03 10:45:19 +0100

    ➕Added docstring to the LoggerUtils class.

--- kwslogger/utils/logger.py
+++ kwslogger/utils/logger.py
@@ -3,5 +3,13 @@ class LoggerUtils:
         pass
 
     def log_to_file(self, message, log_file: str ="mylogs", mode: str ="a"):
+        """
+        Logs the given message to a file.
+
+        Args:
+            message (str): The message to be logged.
+            log_file (str, optional): The name of the log file. Defaults to "mylogs".
+            mode (str, optional): The mode in which the file should be opened. Defaults to "a".
+        """
         with open(f"{log_file}.log", mode, encoding="utf8") as f:
             f.write(f"{message}\n")
